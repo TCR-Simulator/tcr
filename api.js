3b7e341a3c2b40669e955cca3b5a7cc2678d826b
Ece Kayan <ecekayan97@gmail.com>
2018-11-06 15:23:08 +0000

    Initial api skeleton

--- api.js
+++ api.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const app = express();
 
-//app.get(/)
+app.get('/tcrs/', function (req, res) {
+  //Get all TCRs
+});
+
+app.post('/tcrs/', function (req, res) {
+  //Create a new tcr
+	//init_TCR()
+});
+
+app.get('/tcrs/:tcrId', function (req, res) {
+  //Get TCR by id
+});
+
+app.delete('/tcrs/:tcrId', function (req, res) {
+  //Delete TCR by id
+});
+
+
+app.post('/tcrs/:tcrId/deploy', function (req, res) {
+  //Deploy created TCR
+});
