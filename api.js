58aab7beed53e0b322d60a27b410cb1611fcdd58
Yoon Kim <jyk416@ic.ac.uk>
2018-11-06 17:56:06 +0000

    Completed basic methods for TCR api

--- api.js
+++ api.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
+var tcrMap = new Map();
+
+function tcrObject(id, address, name, status, abi, parameters) { 
+	this.id = id;
+	this.address = address;
+	this.name = name;
+	this.status = status;
+	this.abi = abi;
+	this.parameters = parameters;
+}
+
+//Get all TCRs
 app.get('/tcrs/', function (req, res) {
-
-  //Get all TCRs
-	res.send('Hello World!');
+	// console.log(tcrMap.values());
+	res.json(JSON.stringify([...tcrMap]));
 });
 
+//Create a new tcr
 app.post('/tcrs/', function (req, res) {
-  //Create a new tcr
-	//init_TCR()
+	var id = (Math.round((new Date()).getTime() / 1000)).toString();
+	var paramTCR = new tcrObject(id, "0x234981210434", req.body.name, "yet to be deployed",
+		"abi",req.body.parameters);
+	tcrMap.set(id, paramTCR);
+	// res.send(tcrMap.get(id));
+	// console.log(id);
+	res.json(id);
 });
 
 app.get('/tcrs/:tcrId', function (req, res) {
-  //Get TCR by id
+	res.json(tcrMap.get(req.body.id));
 });
 
 app.delete('/tcrs/:tcrId', function (req, res) {
-  //Delete TCR by id
+	tcrMap.delete(req.body.id);
 });
 
 
