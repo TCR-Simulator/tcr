6708c1fb46e194eff2f691cd9713bfe7c6ce1d41
Ece Kayan <ecekayan97@gmail.com>
2018-11-06 15:41:19 +0000

    Server setup code added

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.get('/tcrs/', function (req, res) {
   //Get all TCRs
+	res.send('Hello World!');
 });
 
 app.post('/tcrs/', function (req, res) {
@@ -22,3 +24,5 @@ app.delete('/tcrs/:tcrId', function (req, res) {
 app.post('/tcrs/:tcrId/deploy', function (req, res) {
   //Deploy created TCR
 });
+
+app.listen(port, () => console.log(`TCR Server app listening on port ${port}!`));
