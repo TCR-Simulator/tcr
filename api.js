f992dbb0133105a35fc3ddd5d098a202dca3a9c2
Yoon Kim <jyk416@ic.ac.uk>
2018-11-12 16:39:14 +0000

    Wrote functions to design config files for different TCR parameters

--- api.js
+++ api.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const jsonfile = require('jsonfile');
+const cmd = require('node-cmd');
+const stdin = process.openStdin();
+const fs = require('fs');
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
+
 var tcrMap = new Map();
+
+stdin.addListener("data", function(d) {
+	console.log("you entered: " + d.toString());
+});
 
 function tcrObject(id, address, name, status, abi, parameters) { 
 	this.id = id;
@@ -27,6 +36,13 @@ app.post('/tcrs/', function (req, res) {
 	var id = (Math.round((new Date()).getTime() / 1000)).toString();
 	var paramTCR = new tcrObject(id, "0x234981210434", req.body.name, "yet to be deployed",
 		"abi",req.body.parameters);
+
+	// Create a new config file for the tcr and write parameters inside
+	const config_filename = 'config' + paramTCR.id + '.json';
+	jsonfile.writeFile('./conf/' + config_filename, paramTCR.parameters, function(err) {
+		if (err) console.error(err);
+	});
+	
 	tcrMap.set(id, paramTCR);
 	// res.send(tcrMap.get(id));
 	// console.log(id);
@@ -34,16 +50,26 @@ app.post('/tcrs/', function (req, res) {
 });
 
 app.get('/tcrs/:tcrId', function (req, res) {
-	res.json(tcrMap.get(req.body.id));
+	res.json(tcrMap.get(req.params.tcrId));
 });
 
 app.delete('/tcrs/:tcrId', function (req, res) {
-	tcrMap.delete(req.body.id);
+	const config_name = 'config' + req.params.tcrId + '.json';
+	console.log(config_name);
+	fs.unlink('./conf/' + config_name, (err) => {
+		if (err) console.error(err);
+	});
+	// tcrMap.delete(req.params.tcrId);
 });
 
-
+//Deploy created TCR
 app.post('/tcrs/:tcrId/deploy', function (req, res) {
-  //Deploy created TCR
+	console.log("Deploying tcr - " + JSON.stringify(req.params.tcrId));
+	const config_filepath = './conf/config' + JSON.stringify(req.params.tcrId) + '.json';
+
+	//TODO: find the argv index for config_filename, then you can use process.argv[3] to pass this config file.
+	cmd.run('npm run deploy-proxies:ganache -- ' + config_filepath);
+	
 });
 
 app.listen(port, () => console.log(`TCR Server app listening on port ${port}!`));
--- truffle.js
+++ truffle.js
@@ -12,6 +12,8 @@ if (process.env.MNEMONIC) {
   mnemonic = process.env.MNEMONIC;
 }
 
+mnemonic = 'cable bounce labor unknown exist movie cheap dilemma nose market foil elite';
+
 module.exports = {
   networks: {
     mainnet: {
@@ -25,6 +27,7 @@ module.exports = {
       network_id: '*',
       gas: 6000000,
       gasPrice: 25000000000,
+      host: "localhost"
     },
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
@@ -42,7 +45,7 @@ module.exports = {
     coverage: {
       host: 'localhost',
       network_id: '*',
-      port: 7545, // <-- If you change this, also set the port option in .solcover.js.
+      port: 8545, // <-- If you change this, also set the port option in .solcover.js.
       gas: 0xfffffffffff, // <-- Use this high gas value
       gasPrice: 0x01, // <-- Use this low gas price
     },
